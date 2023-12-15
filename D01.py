59d5e55e2e66840e880d3680858043f7afc8eca3
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2023-12-15 15:35:34 +1100

    Part Two の途中まで解いた。

---
+++ D01.py
@@ -0,0 +1,26 @@
+import re
+
+def replace_with_digits(str):
+    # Python ではハッシュは辞書オブジェクトと呼ぶらしい。
+    digits = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    for key in digits:
+        str = re.sub(key, digits[key], str)
+    return str
+
+file = 'input.txt'
+file = 'example-2.txt'
+replace_numbers = True
+sum = 0
+with open(file) as f:
+    lines = [s.rstrip() for s in f.readlines()]
+    for line in lines:
+        if replace_numbers:
+            line = replace_with_digits(line)
+
+        print(line)
+        first = re.search(r'\d', line).group()
+        last_plus = re.search(r'(\d)\D*$', line).group()
+        last = re.match(r'\d', last_plus).group()
+        num = int(first + last)
+        sum += num
+print(sum)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Advent-2023
-https://adventofcode.com/2023/
+
+My solutions to https://adventofcode.com/2023/ in Python.
+
+[Pythonに関する情報 | note.nkmk.me](https://note.nkmk.me/python/) worked well for me as a reference.
