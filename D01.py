0c24c90bf0e8b8c409b4361b392a0ccf575dbbd6
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2023-12-20 14:45:56 +1100

    Add a comment.

--- D01.py
+++ D01.py
@@ -1,5 +1,15 @@
 import re
 
+# "eightwo" の様にふたつの数が繋がっている場合は、左を優先的にアラビア数字に変換する必要がある。
+#
+# one eight
+# two one
+# three eight
+# five eight
+# seven nine
+# eight two
+# nine eight
+
 def replace_with_digits(str):
     # Python ではハッシュは辞書オブジェクトと呼ぶらしい。
     digits = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
