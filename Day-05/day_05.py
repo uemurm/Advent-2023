a6dbe2ab62e82330d8e4defec7450c28d70414cf
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2023-12-24 13:51:24 +1100

    Use a debug option from command line.

--- Day-05/day_05.py
+++ Day-05/day_05.py
@@ -2,18 +2,16 @@
 Day 05: Transform seed numbers to their locations.
 """
 import re
+import argparse
 
 FILE_NAME = 'Day-05/input.txt'
 FILE_NAME = 'Day-05/example-1.txt'
-FILE_NAME = 'Day-05/example-0.txt'
+# FILE_NAME = 'Day-05/example-0.txt'
 
 class MapList(list):
     """
     A list of maps to map seed numbers to their locations.
     """
-
-    def __init__(self):
-        print("init")
 
     def input(self, num):
         """
@@ -24,21 +22,22 @@ class MapList(list):
             v = my_map.get(v, v)   # Return the same value unless the key exists.
         return v
 
-ml = MapList()
+parser = argparse.ArgumentParser(description='Transform seed numbers to their locations.')
+parser.add_argument('-d', '--debug', help='Print debug info.', action='store_true')
+args = parser.parse_args()
+
+maps = MapList()
 
 f = open(FILE_NAME, 'r', -1, 'UTF-8')
 lines = f.read().splitlines()
 
 IS_MAP = False
-m = {}
-for line in lines:
+for row, line in enumerate(lines, 1):
     if IS_MAP and len(line) == 0:
         IS_MAP = False
-        print('before:\n', ml)
-        print('m:\n', m)
-        ml.append(m)
-        print('after:\n', ml)
-        # print("\n")
+        if args.debug:
+            print('m:\n', m)
+        maps.append(m)
 
     if IS_MAP:
         dst_st, src_st, length = map(int, re.findall(r'\d+', line))
@@ -47,6 +46,9 @@ for line in lines:
 
     if re.search(r'map:$', line):
         IS_MAP = True
-        m.clear()
-# print(ml)
-# print(ml.input(79))
+        m = {}
+
+    if args.debug:
+        print(row, 'maps:\n', maps)
+
+print(maps.input(79))
